9bd1505242e6e5ee61a5e2e8829b3a1a007f7e9f
Tuff <88716247+ffuT@users.noreply.github.com>
2024-07-19 23:48:37 +0200

    initial

---
+++ premake5.lua
@@ -0,0 +1,41 @@
+workspace "MyOpenGl"
+    configurations { "Debug", "Release" }
+    platforms { "x64" }  -- Specify x64 platform
+    location "Build"  -- Generated files will go here
+
+    -- Platform-specific settings
+    filter "system:windows"
+        architecture "x64"
+        systemversion "latest"
+
+    filter "system:linux"
+        architecture "x64"
+        toolset "gcc"
+
+    filter {}
+
+project "MyOpenGl"
+    kind "ConsoleApp"
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}"
+
+    files { "src/**.cpp", "src/**.h", "src/**.hpp", "headers/**.h", "headers/**.h" }  -- Source and header files
+
+    includedirs { "include", "Dependencies/glfw/include", "Dependencies/glew/include", "src/vendor" }
+
+    -- Link GLFW and GLEW libraries
+    filter "system:windows"                             
+        libdirs { "Dependencies/glfw/lib-vc2022", "Dependencies/glew/lib/Release/x64" }
+        links { "glfw3", "glew32", "opengl32" }  -- OpenGL library for Windows
+
+    filter "system:linux"
+        links { "glfw", "GLEW", "GL" }  -- OpenGL library for Linux
+
+    -- Debug and Release configuration
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
