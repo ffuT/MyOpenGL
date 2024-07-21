504cf5286a1a5d388884d45ebce7a4b8e2185662
Tuff <88716247+ffuT@users.noreply.github.com>
2024-07-21 02:33:57 +0200

    Sphere, Shader object
    TODO: abstractions for opengl shiiiiet

--- res/shaders/BasicShader.shader
+++ res/shaders/BasicShader.shader
@@ -1,14 +1,20 @@
 #shader vertex
 #version 330 core
 
-layout(location = 0) in vec4 position;
+layout(location = 0) in vec3 aPos;
+
+uniform mat4 u_model;
+uniform mat4 u_view;
+uniform mat4 u_proj;
 
 void main(){
-	gl_Position = position;
+	gl_Position =  u_proj * u_view * u_model * vec4(aPos, 1.0);
 };
 
 #shader fragment
 #version 330 core
+
+out vec4 Color;
 
 void main(){
 	Color = vec4(1.0, 0.0, 0.0, 1.0);
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -10,7 +11,22 @@
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 
+#include "Shader.h"
+
+float* CreateSphere(const float radius, const int PointAmount);
+unsigned int* CreateSphereIndices(const int PointAmount);
+
 constexpr int WIDTH = 1280, HEIGHT = 720;
+
+glm::mat4 model = glm::mat4(1.0f);
+glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+glm::mat4 view = glm::lookAt(glm::vec3(0.0, 0.0, 50.0),
+                             glm::vec3(0.0, 0.0, 0.0),
+                             glm::vec3(0.0, 1.0, 0.0));
+
+float *vertices = CreateSphere(5, 48);
+
+unsigned int* indices = CreateSphereIndices(48);
 
 int main(void)
 {
@@ -42,19 +58,50 @@ int main(void)
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330 core");
 
+    glViewport(0, 0, WIDTH, HEIGHT);
+    glfwWindowHint(GLFW_DEPTH_BITS, 24);
+    glEnable(GL_DEPTH_TEST);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, (3 * 48 * 48) * sizeof(float), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, (6 * 47 * 47) * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    Shader shader("../res/shaders/BasicShader.shader");
+    shader.Bind();
+    shader.SetUniformMat4f("u_view", view);
+    shader.SetUniformMat4f("u_proj", proj);
+    shader.SetUniformMat4f("u_model", model);
+    shader.Bind();
+    glBindVertexArray(VAO);
+
     auto now = std::chrono::system_clock::now();
     auto last = std::chrono::system_clock::now();
-    auto second = now + std::chrono::seconds(1);
     float delta = 0.0f;
     while (!glfwWindowShouldClose(window))
     {
         last = now;
         now = std::chrono::system_clock::now();
-        delta = (float)(now - last).count() / 10000000;
+        delta = (float) (now - last).count() * 0.0000001;
         static float color[4] = {0.1, 0.1, 0.1, 1.0};
         glClearColor(color[0], color[1], color[2], color[3]);
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
@@ -69,10 +116,65 @@ int main(void)
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
+        shader.SetUniformMat4f("u_view", view);
+        shader.SetUniformMat4f("u_proj", proj);
+        shader.SetUniformMat4f("u_model", model);
+
+        //RENDER SPHERE
+        glDrawElements(GL_TRIANGLES, 6 * (48-1) * (48 - 1), GL_UNSIGNED_INT, 0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
     glfwTerminate();
     return 0;
 }
+
+constexpr float PI = 3.1415926535897932;
+
+float* CreateSphere(const float radius, const int PointAmount) {
+    const int totalelements = 3 * PointAmount * PointAmount; // Adjusted for more points
+
+    float* points = new float[totalelements];
+
+    int index = 0;
+    for (size_t i = 0; i < PointAmount; i++) {
+        float phi = PI * i / (PointAmount - 1); // Ranges from 0 to PI
+
+        for (size_t j = 0; j < PointAmount; j++) {
+            float theta = 2 * PI * j / (PointAmount - 1); // Ranges from 0 to 2*PI
+
+            points[index++] = radius * sin(phi) * cos(theta);
+            points[index++] = radius * sin(phi) * sin(theta);
+            points[index++] = radius * cos(phi);
+        }
+    }
+    return points;
+} //thanks gpt
+
+unsigned int* CreateSphereIndices(const int PointAmount) {
+    const int totalIndices = 6 * (PointAmount - 1) * (PointAmount - 1);
+    unsigned int* indices = new unsigned int[totalIndices];
+
+    int index = 0;
+    for (int i = 0; i < PointAmount - 1; i++) {
+        for (int j = 0; j < PointAmount - 1; j++) {
+            int current = i * PointAmount + j;
+            int next = current + PointAmount;
+
+            indices[index++] = current;
+            indices[index++] = next;
+            indices[index++] = current + 1;
+
+            indices[index++] = current + 1;
+            indices[index++] = next;
+            indices[index++] = next + 1;
+        }
+    }
+    return indices;
+} //thanks gpt
