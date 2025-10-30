7d401903e28235624e57f7fa8b570d19b1aba624
Tuff <88716247+ffuT@users.noreply.github.com>
2025-10-30 21:55:38 +0100

    mhm

--- src/physics/RigidBody.h
+++ src/physics/RigidBody.h
@@ -15,11 +15,7 @@ struct RigidBody {
 	glm::vec3 velocity = glm::vec3(0.0);
 	glm::vec3 forces = glm::vec3(0.0);
 
-<<<<<<< HEAD
-	float restitution = 0.7f; // bounciness
-=======
 	float restitution = 0.75f; // bounciness
->>>>>>> 7a96299e74eded38d589449c5fd8b6736814f5a3
 	ColliderType colliderType = ColliderType::Sphere;
 	float radius = 1.0f;
 	glm::vec3 halfExtents = glm::vec3(1.0f);
